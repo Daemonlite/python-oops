e799034f1c93fe27d1988046b06cd4e24a8f44b9
Eugene <khemikal2016@gmail.com>
2023-10-25 08:18:27 +0000

    expiry class updated

--- adv/expiry.py
+++ adv/expiry.py
@@ -1,34 +1,28 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 import arrow
-    
+
+
 class ExpirationChecker:
-    def __init__(self,month,year):
+    def __init__(self, month, year):
         self.month = month
         self.year = year
-    
-    
+
     def check_expiration(self):
         current_date = datetime.datetime.now().date()
         six_months_from_now = current_date + relativedelta(months=6)
-        three_months_from_now = current_date + relativedelta(months=3)
-        
-       
-        expiry_date = datetime.datetime(self.year,self.month, 1)
+        # three_months_from_now = current_date + relativedelta(months=3)
+
+        expiry_date = datetime.datetime(self.year, self.month, 1)
         arr = arrow.get(expiry_date).humanize()
 
         if current_date <= expiry_date.date() <= six_months_from_now:
-            
+
             print(f"Item expiring : {arr}")
-        
-        
+
         if current_date > expiry_date.date():
             print(f"Item expired: {arr}")
 
 
-
-    
-
 checker = ExpirationChecker(10, 2023)
 exp = checker.check_expiration()
-
