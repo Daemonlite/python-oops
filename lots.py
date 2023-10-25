12217bba8e3d4705c2996db6ed5ebb49c6339366
Eugene <khemikal2016@gmail.com>
2023-10-25 13:42:54 +0000

    updated lot

--- lots.py
+++ lots.py
@@ -1,15 +1,27 @@
 from adv.calc import Random
 
-rand = Random(5)
+def play_lottery():
+    while True:
+        inp = input("Enter 5 unique lucky numbers between 1 and 90 (no duplicates): ")
+        inp = inp.strip().split(",")
+        if len(inp) != 5:
+            print("Please enter exactly 5 numbers.")
+            continue
+        try:
+            inp = list(map(int, inp))
+            if len(set(inp)) == 5:
+                rand = Random(5)
+                outcome = rand.random_number()
+                if set(outcome) == set(inp):
+                    print("You won!")
+                else:
+                    print(f"You lost. Winning numbers are: {outcome}")
+                break
+            else:
+                print("Please enter 5 unique numbers.")
+        except ValueError:
+            print("Invalid input. Please enter numbers separated by commas.")
 
-inp = input("enter 5 lucky numbers between 1 and 90 : ")
 
-inp.split(",")
-
-outcome = rand.random_number()
-
-if all(inp) in outcome:
-    print("you won")
-else:
-    print(f"You lost, winning numbers are : {outcome}")
-
+for i in range(3):
+    play_lottery()
